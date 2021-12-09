8756f0205a78d77e65cdd7f002b2cce272ece93d
naama322259128 <82829409+naama322259128@users.noreply.github.com>
2021-12-09 21:09:56 +0200

    change 2

--- src/hooks/usePeopleFetch.js
+++ src/hooks/usePeopleFetch.js
@@ -12,10 +12,10 @@ export const usePeopleFetch = () => {
 
   async function fetchUsers() {
     setIsLoading(true);
-    // const response = await axios.get(`https://randomuser.me/api/?results=25&page=1`);
+    const response = await axios.get(`https://randomuser.me/api/?results=25&page=1`);
     setIsLoading(false);
-    setUsers(results);
-    // setUsers(response.data.results);
+    // setUsers(results);
+   setUsers(response.data.results);
   }
 
   return { users, isLoading, fetchUsers };
